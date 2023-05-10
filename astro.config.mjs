020f9c2f41bff999761085283537bf3fb65ebcd3
Koji Uegaki <kjugkjk@gmail.com>
2023-05-10 22:11:16 +0900

    deploy 用の github actions を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kjugk.github.io",
+  base: "my-astro-blog",
   integrations: [
     tailwind({
       config: {
